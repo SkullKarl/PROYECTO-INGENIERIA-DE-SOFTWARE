2ebda5da9aa7db43d2c536cef5dfddd3edb7bc9e
Eshnaider <pantostado1601@gmail.com>
2024-11-18 10:44:08 -0300

    Endpoints /List + /Get_by_id para agenda de citas

--- API/routes/appointment_routes.py
+++ API/routes/appointment_routes.py
@@ -3,6 +3,7 @@
 from bson import ObjectId
 from flask import Blueprint, request, jsonify
 from models.appointment import Appointment
+from config import get_db
 
 appointment_bp = Blueprint('appointment', __name__)
 
@@ -35,3 +36,64 @@ def schedule_appointment():
     response_data = convert_objectid(response[0])
 
     return jsonify(response_data), response[1]
+
+# Obtener todas las citas
+@appointment_bp.route('/List', methods=['GET'])
+def get_all_appointments():
+    db = get_db()
+    appointments_collection = db["appointment"]
+    
+    appointments = list(appointments_collection.find())
+    
+    # Convertir todos los ObjectId a cadenas
+    def convert_objectid(obj):
+        if isinstance(obj, dict):
+            return {key: convert_objectid(value) for key, value in obj.items()}
+        elif isinstance(obj, list):
+            return [convert_objectid(item) for item in obj]
+        elif isinstance(obj, ObjectId):
+            return str(obj)
+        return obj
+
+    appointments_data = convert_objectid(appointments)
+    
+    return jsonify(appointments_data), 200
+
+# Obtener una cita por ID
+@appointment_bp.route('/Get_by_id', methods=['POST'])
+def get_appointment_by_id():
+
+    data = request.get_json()
+    appointment_id = data.get('appointment_id')
+
+    if not appointment_id:
+        return jsonify({"error": "El campo 'appointment_id' es requerido"}), 400
+
+    db = get_db()
+    appointments_collection = db["appointment"]
+
+    try:
+        appointment_id = ObjectId(appointment_id)
+        appointment = appointments_collection.find_one({"_id": appointment_id})
+        if not appointment:
+            return jsonify({"error": "Cita no encontrada"}), 404
+
+        # ... (resto del código para convertir ObjectId a str y retornar la respuesta)
+
+    except (TypeError, ValueError):
+        return jsonify({"error": "ID invalido"}), 400
+    
+    # Convertir todos los ObjectId a cadenas
+    def convert_objectid(obj):
+        if isinstance(obj, dict):
+            return {key: convert_objectid(value) for key, value in obj.items()}
+        elif isinstance(obj, list):
+            return [convert_objectid(item) for item in obj]
+        elif isinstance(obj, ObjectId):
+            return str(obj)
+        return obj
+
+    appointments_data = convert_objectid(appointment)
+    
+    return jsonify(appointments_data), 200
+    
